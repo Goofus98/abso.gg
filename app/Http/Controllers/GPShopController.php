7c8449109b0a0be7e4258d56bc658dfa6f19e5d5
Goofus <jbellido158@gmail.com>
2025-07-16 14:10:14 -0700

    Fix Input

--- app/Http/Controllers/GPShopController.php
+++ app/Http/Controllers/GPShopController.php
@@ -10,20 +10,20 @@ class GPShopController extends Controller
     public function auction(Request $request, GPShopService $shopService)
     {
         
+        $isBidding = $request->input('is_bidding');
+        $uid = $request->input('uid');
+        $itemId = $request->input('item_id');
+        
+        $price = $request->input('price');
+        $duration = $request->input('duration');
+        $purchaseData = $request->input('purchase_data');
+
+        $encodedMods = $request->input('mods');
+        $mods = base64_decode($encodedMods);
+        $inventory = base64_decode($request->input('inventory'));
+
         $payload = $request->attributes->get('jwt_payload');
         $steamId = $payload->steamid;
-
-        $isBidding = (bool) $request->input('is_bidding');
-        $uid = (int) $request->input('uid');
-        $itemId = (int) $request->input('item_id');
-        
-        
-        $price = (int) $request->input('price');
-        $duration = (int) $request->input('duration');
-        $purchaseData = $request->input('purchase_data');
-        $mods = $request->input('mods');
-        $inventory = $request->input('inventory');
-        
 
         $auction = $shopService->createAuction(
             $steamId,
@@ -37,6 +37,7 @@ class GPShopController extends Controller
             $inventory
         );
 
-        return response()->json(['auction' => $auction]);
+        $auction["Mods"] = $encodedMods;
+        return response()->json([$auction]);
     }
 }
